ed1f31db113ce55d956ca854c6020aac0e9fca5e
Shaojun Bao <youngwolf111@gmail.com>
2023-08-13 16:45:37 -0400

    finished finch lab

--- main_app/models.py
+++ main_app/models.py
@@ -2,12 +2,29 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
 
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
+
 
     def __str__(self):
       return f'{self.name} ({self.id})'
@@ -15,3 +32,15 @@ class Finch(models.Model):
   # Add this method
     def get_absolute_url(self):
       return reverse('detail', kwargs={'finch_id': self.id})
+    
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    choices = MEALS,
+    default=MEALS[0][0])
+  
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
